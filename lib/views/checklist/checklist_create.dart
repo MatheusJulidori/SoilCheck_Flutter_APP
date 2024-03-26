44f82f1c8001635858e1c661129d1aa5754b7070
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-03-26 19:14:28 -0300

    Create Checklist

--- lib/views/checklist/checklist_create.dart
+++ lib/views/checklist/checklist_create.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:soilcheck/models/checklist.dart';
+import 'package:soilcheck/models/cliente.dart';
+import 'package:soilcheck/models/fazenda.dart';
+import 'package:soilcheck/models/pivo.dart';
 import 'package:soilcheck/models/template.dart';
 import 'package:soilcheck/providers/auth_provider.dart';
 import 'package:soilcheck/providers/checklist_provider.dart';
+import 'package:soilcheck/providers/cliente_provider.dart';
+import 'package:soilcheck/providers/fazenda_provider.dart';
+import 'package:soilcheck/providers/pivo_provider.dart';
 import 'package:soilcheck/providers/template_provider.dart';
 
 class ChecklistField {
@@ -25,15 +31,40 @@ class CreateChecklist extends StatefulWidget {
 }
 
 class _CreateChecklistState extends State<CreateChecklist> {
-  final TextEditingController _fazendaController = TextEditingController();
-  final TextEditingController _pivoController = TextEditingController();
   final TextEditingController _idRadioController = TextEditingController();
   final TextEditingController _observacoesGeraisController =
       TextEditingController();
-  Template? _selectedTemplate;
   final List<ChecklistField> _checklistFields = [];
-  Iterable<Template>? _filteredAutocompletions;
-  String _searchingWithQuery = '';
+
+  Template? _selectedTemplate;
+  Iterable<Template>? _filteredTemplatesCompletions;
+  String _templateQuery = '';
+
+  Cliente? _selectedCliente;
+  Iterable<Cliente>? _filteredClientesCompletions;
+  final String _clienteQuery = '';
+
+  Fazenda? _selectedFazenda;
+  List<Fazenda> fazendaList = [];
+  final List<Fazenda> filteredFazendaList = [];
+
+  Pivo? _selectedPivo;
+  List<Pivo> pivoList = [];
+  final List<Pivo> filteredPivoList = [];
+
+  void _fetchFazendasAndPivos() async {
+    var fazendas = await Provider.of<FazendaProvider>(context, listen: false)
+        .getAllFazendas();
+    var pivos =
+        await Provider.of<PivoProvider>(context, listen: false).getAllPivos();
+
+    if (mounted) {
+      setState(() {
+        fazendaList = fazendas;
+        pivoList = pivos;
+      });
+    }
+  }
 
   void _initializeChecklistFields() {
     _checklistFields.clear();
@@ -51,6 +82,39 @@ class _CreateChecklistState extends State<CreateChecklist> {
     }
 
     setState(() {});
+  }
+
+  void _updateFazendasList() {
+    filteredFazendaList.clear();
+    _selectedFazenda = null;
+    _selectedPivo = null;
+
+    if (_selectedCliente != null) {
+      for (var fazenda in fazendaList) {
+        if (fazenda.idCliente == _selectedCliente!.id!) {
+          filteredFazendaList.add(fazenda);
+        }
+      }
+    }
+  }
+
+  void _updatePivosList() {
+    filteredPivoList.clear();
+    _selectedPivo = null;
+
+    if (_selectedFazenda != null) {
+      for (var pivo in pivoList) {
+        if (pivo.idFazenda == _selectedFazenda!.id!) {
+          filteredPivoList.add(pivo);
+        }
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchFazendasAndPivos();
   }
 
   @override
@@ -74,16 +138,16 @@ class _CreateChecklistState extends State<CreateChecklist> {
             children: [
               Autocomplete<Template>(
                 optionsBuilder: (TextEditingValue textEditingValue) async {
-                  _searchingWithQuery = textEditingValue.text;
+                  _templateQuery = textEditingValue.text;
                   final Iterable<Template> options = (await context
                       .read<TemplateProvider>()
-                      .getTemplatesWithFilter(_searchingWithQuery));
-                  if (_searchingWithQuery != textEditingValue.text) {
-                    if (_filteredAutocompletions != null) {
-                      return _filteredAutocompletions!;
+                      .getTemplatesWithFilter(_templateQuery));
+                  if (_templateQuery != textEditingValue.text) {
+                    if (_filteredTemplatesCompletions != null) {
+                      return _filteredTemplatesCompletions!;
                     }
                   }
-                  _filteredAutocompletions = options;
+                  _filteredTemplatesCompletions = options;
                   return options;
                 },
                 displayStringForOption: (Template option) => option.name,
@@ -93,17 +157,92 @@ class _CreateChecklistState extends State<CreateChecklist> {
                     _initializeChecklistFields();
                   });
                 },
+                fieldViewBuilder:
+                    (context, controller, focusNode, onFieldSubmitted) {
+                  return TextField(
+                    controller: controller,
+                    focusNode: focusNode,
+                    decoration: const InputDecoration(
+                      hintText: "Selecione o Template",
+                    ),
+                  );
+                },
               ),
-              TextField(
-                controller: _fazendaController,
-                decoration: const InputDecoration(
-                  hintText: "Fazenda",
+              Autocomplete<Cliente>(
+                optionsBuilder: (TextEditingValue textEditingValue) async {
+                  _templateQuery = textEditingValue.text;
+                  final Iterable<Cliente> options = (await context
+                      .read<ClienteProvider>()
+                      .getClientesWithFilter(_clienteQuery));
+                  if (_clienteQuery != textEditingValue.text) {
+                    if (_filteredClientesCompletions != null) {
+                      return _filteredClientesCompletions!;
+                    }
+                  }
+                  _filteredClientesCompletions = options;
+                  return options;
+                },
+                displayStringForOption: (Cliente option) => option.name,
+                onSelected: (Cliente selection) {
+                  setState(() {
+                    _selectedCliente = selection;
+                    _updateFazendasList();
+                  });
+                },
+                fieldViewBuilder:
+                    (context, controller, focusNode, onFieldSubmitted) {
+                  return TextField(
+                    controller: controller,
+                    focusNode: focusNode,
+                    decoration: const InputDecoration(
+                      hintText: "Selecione o Cliente",
+                    ),
+                  );
+                },
+              ),
+              Container(
+                width: double.infinity,
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<Fazenda>(
+                    isExpanded: true,
+                    value: _selectedFazenda,
+                    hint: const Text("Selecione a Fazenda"),
+                    onChanged: (Fazenda? newValue) {
+                      setState(() {
+                        _selectedFazenda = newValue;
+                        _updatePivosList();
+                      });
+                    },
+                    items: filteredFazendaList
+                        .map<DropdownMenuItem<Fazenda>>((Fazenda fazenda) {
+                      return DropdownMenuItem<Fazenda>(
+                        value: fazenda,
+                        child: Text(fazenda.name),
+                      );
+                    }).toList(),
+                  ),
                 ),
               ),
-              TextField(
-                controller: _pivoController,
-                decoration: const InputDecoration(
-                  hintText: "Pivo",
+              Container(
+                width: double.infinity,
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<Pivo>(
+                    isExpanded: true,
+                    value: _selectedPivo,
+                    hint: const Text("Selecionar Pivo"),
+                    onChanged: (Pivo? newValue) {
+                      setState(() {
+                        _selectedPivo = newValue;
+                      });
+                    },
+                    items: filteredPivoList
+                        .map<DropdownMenuItem<Pivo>>((Pivo pivo) {
+                      return DropdownMenuItem<Pivo>(
+                        value: pivo,
+                        child: Text(pivo.name),
+                      );
+                    }).toList(),
+                  ),
                 ),
               ),
               TextField(
@@ -122,13 +261,17 @@ class _CreateChecklistState extends State<CreateChecklist> {
                 onPressed: () async {
                   final bool areRequiredFilled;
                   if (_checklistFields.isNotEmpty) {
-                    areRequiredFilled = _checklistFields.every(
-                        (field) => (field.isRequired && field.observation!.isEmpty) ? false : true);
+                    areRequiredFilled = _checklistFields.every((field) =>
+                        (field.isRequired && field.observation!.isEmpty)
+                            ? false
+                            : true);
                   } else {
                     areRequiredFilled = true;
                   }
                   if (_idRadioController.text.isNotEmpty &&
-                      areRequiredFilled) {
+                      areRequiredFilled &&
+                      _selectedFazenda != null &&
+                      _selectedPivo != null) {
                     final fieldsFormatted = _checklistFields
                         .map((field) => {
                               'fieldName': field.fieldName,
@@ -140,8 +283,8 @@ class _CreateChecklistState extends State<CreateChecklist> {
 
                     Checklist newChecklist = Checklist(
                       idTemplate: _selectedTemplate!.id!,
-                      idFazenda: _fazendaController.text,
-                      idPivo: _pivoController.text,
+                      idFazenda: _selectedFazenda!.id!,
+                      idPivo: _selectedPivo!.id!,
                       idRadio: _idRadioController.text,
                       observacoesGerais: _observacoesGeraisController.text,
                       revisao: '',
@@ -162,8 +305,9 @@ class _CreateChecklistState extends State<CreateChecklist> {
                       );
 
                       _idRadioController.clear();
-                      _fazendaController.clear();
-                      _pivoController.clear();
+                      _selectedCliente = null;
+                      _selectedFazenda = null;
+                      _selectedPivo = null;
                       _observacoesGeraisController.clear();
                       setState(() => _checklistFields.clear());
                     } catch (e) {
